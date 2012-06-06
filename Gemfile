10cb4ff40f61f99348806906926e18d63698295f
Neta <netadvd@gmail.com>
2012-06-06 12:04:06 +0300

    Message

--- Gemfile
+++ Gemfile
@@ -34,6 +34,10 @@ group :production do
   gem 'pg' , '0.12.2'
 end
 
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+gem 'bootstrap-sass', '2.0.0'
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  
+  root :to => 'static_pages#home'
 
-  get "static_pages/help"
   
-  get "static_pages/about"
+  match '/help' => 'static_pages#help'
   
-  get "static_pages/contact"
+  match '/about' => 'static_pages#about'
+  
+  match '/contact' => 'static_pages#contact'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
