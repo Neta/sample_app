2797fb63540917765798d9cc7bffc5d97286bfa2
Neta <netadvd@gmail.com>
2012-06-06 12:55:04 +0300

    Finish layout and routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
   
+  get "users/new"
+
   root :to => 'static_pages#home'
 
+  match '/signup' => 'users#new'
   
   match '/help' => 'static_pages#help'
   
