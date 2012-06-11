ac15fec0177ecf9ad1b92abd7ae95ca84fcf429d
Neta <netadvd@gmail.com>
2012-06-11 13:33:43 +0300

    Add SSL in production

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,14 +15,14 @@ class User < ActiveRecord::Base
   
    before_save { self.email = email.downcase }
   
-  validates :name, :presence=> true, :length=> { :maximum=> 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  
-  validates :email, :presence=> true, 
-            :format=> { :with=> VALID_EMAIL_REGEX },
-            :uniqueness=> { :case_sensitive=> false }
-  
-  validates :password, :presence=> true, :length=> { :minimum=>6 }       
-  
-  validates :password_confirmation, :presence=> true   
+  # validates :name, :presence=> true, :length=> { :maximum=> 50 }
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+#   
+  # validates :email, :presence=> true, 
+            # :format=> { :with=> VALID_EMAIL_REGEX },
+            # :uniqueness=> { :case_sensitive=> false }
+#   
+  # validates :password, :length=> { :minimum=>6 }       
+#   
+  # validates :password_confirmation, :presence=> true   
 end
